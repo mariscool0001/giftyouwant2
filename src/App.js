5789ee1a626ca529566bf921baad1b8d10ae3a7b
mariscool0001 <marthasdominguezg@gmail.com>
2023-06-22 10:59:58 +0100

    commit 01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Navbar';
+import Home from './pages/Home';
+import Familymembers from './pages/familymembers';
+import Friends from './pages/friends';
+import Anniversaries from './pages/Anniversaries';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Nav/>
+     <Routes>
+      
+      <Route path='/Home'element={<Home/>}/>
+      <Route path='/familymembers'element={<Familymembers/>}/>
+      <Route path='/friends'element={<Friends/>}/>
+      <Route path='/anniversaries'element={<Anniversaries/>}/>
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+      <Route></Route>
+     </Routes>
     </div>
   );
 }
